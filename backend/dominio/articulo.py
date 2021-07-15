5768c7cf8676657f78bbf9f46f9ab028aa0bafbd
emevazquez <milivazquezjeje@gmail.com>
2021-07-15 12:27:46 -0300

    Update articulo.py

---
+++ backend/dominio/articulo.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, Date, Boolean, Float
+from sqlalchemy.orm import relationship
+from backend.datos import db
+class Articulo(db.Model):
+    __tablename__ = 'articulos'
+    codigo = Column(Integer, primary_key=True, autoincrement=True)
+    descripcion = Column(String(20), nullable=False)
+    precio = Column(Float(), nullable=False)
