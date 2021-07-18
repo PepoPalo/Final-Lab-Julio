1b9e8c688d1c9fca4657f80862cc48d8dc4579f3
emevazquez <milivazquezjeje@gmail.com>
2021-07-18 13:56:42 -0300

    modelando domiinio faltante

---
+++ backend/dominio/cliente.py
@@ -0,0 +1,14 @@
+
+from sqlalchemy import Column, Integer, String, Date, Boolean, Float
+from sqlalchemy.orm import relationship
+from backend.datos import db
+class Cliente(db.Model):
+    __tablename__ = 'clientes'
+    # enunciado pide
+    # (nombre, dir, tel, cuit, localidad)
+    codigo = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(40), nullable=False)
+    direccion = Column(String(80), nullable=False)
+    telefono = Column(String(40),nullable=False)
+    cuit = Column(String(40), nullable=False)
+    localidad = Column(String(40), nullable=False)
--- backend/dominio/facturaDetalle.py
+++ backend/dominio/facturaDetalle.py
@@ -1,2 +1,14 @@
 # cantidad, codigo de producto, descripcion de producto, precio unitario, porcentaje de iva aplicado (21 o
 # 10.5), subtotal.
+from sqlalchemy import Column, Integer, String, Date, Boolean, Float
+from sqlalchemy.orm import relationship
+from backend.datos import db
+class FacturaDetalle(db.Model):
+    # codigo - descripcion - cantidad vendida - monto total (con iva)
+    __tablename__ = 'facturas_detalle'
+    #Relacion con Articulo/Producto?
+    
+    codigo = Column(Integer, primary_key=True, autoincrement=True)
+    articulo = relationship
+    cantidad = Column(Integer(), nullable=False)
+    total = Column(Float(),nullable=False)
