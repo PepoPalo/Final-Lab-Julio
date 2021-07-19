284e68607e39c5f2ea5f41ef1e517febffae8cc8
emevazquez <milivazquezjeje@gmail.com>
2021-07-19 16:38:29 -0300

    a ver esas relaciones

---
+++ backend/api/facturasVenta_api.py
@@ -0,0 +1,113 @@
+from flask import abort
+from flask_restx import Resource, Namespace, Model, fields, reqparse
+from infraestructura.facturasVenta_repo import FacturasVentaRepo
+from infraestructura.vendedores_repo import VendedoresRepo
+from flask_restx.inputs import date
+
+repo = FacturasVentaRepo()
+repoVendedor = VendedoresRepo()
+
+
+nsFacturaVenta = Namespace('FacturasVenta', description='Administrador de facturas de venta')
+
+modeloFacturaVentaSinID = Model('FacturaDetalleSinCod',{
+    'tipo_id': fields.Integer(),
+    'vendedor_id': fields.Integer(),
+    'cliente_id': fields.Integer(),
+    'cliente': fields.Integer(),
+    'detalle': fields.Float(),
+    'total': fields.Float(),
+    'fecha': fields.Date()
+    # ?????????????????
+    # 'vendedor': fields.ClassName(),
+    # 'cliente':
+    # 'tipo':
+    # 'detalle':
+    
+})
+
+modeloFacturaVenta = modeloFacturaVentaSinID.clone('FacturaDetalle',{
+    'numero': fields.Integer(),
+
+})
+# modeloBusqueda = Model('BusquedaFechas', {
+#     'desde': fields.Date(),
+#     'hasta': fields.Date()
+# })
+
+nsFacturaVenta.models[modeloFacturaVenta.name] = modeloFacturaVenta
+nsFacturaVenta.models[modeloFacturaVentaSinID.name] = modeloFacturaVentaSinID
+# nsFacturaVenta.models[modeloBusqueda.name] = modeloBusqueda
+
+nuevoFacturaVentaParser = reqparse.RequestParser(bundle_errors=True)
+nuevoFacturaVentaParser.add_argument('tipo_id', type=int, required=True)
+nuevoFacturaVentaParser.add_argument('vendedor_id', type=int, required=True)
+nuevoFacturaVentaParser.add_argument('cliente_id', type=int, required=True)
+
+# # # ver como declaramos objetos vendedor, cliente, tipo(factura), detalle
+################################################################################
+# nuevoFacturaVentaParser.add_argument('cliente', type=?, required=True)
+# nuevoFacturaVentaParser.add_argument('vendedor', type=?, required=True)
+# nuevoFacturaVentaParser.add_argument('tipo', type=?, required=True)
+# nuevoFacturaVentaParser.add_argument('detalle', type=float, required=True)
+################################################################################
+
+
+nuevoFacturaVentaParser.add_argument('total', type=float, required=True)
+
+
+@nsFacturaVenta.route('/')
+class FacturaVentaResource(Resource):
+    @nsFacturaVenta.marshal_list_with(modeloFacturaVenta)
+    def get(self):
+        return repo.get_all()
+
+    @nsFacturaVenta.expect(modeloFacturaVentaSinID)
+    @nsFacturaVenta.marshal_with(modeloFacturaVenta)
+    def post(self):
+        data = nuevoFacturaVentaParser.parse_args()
+        p = repo.agregar(data)
+        if p:
+            return p, 200
+        abort(500)
+
+@nsFacturaVenta.route('/<int:id>')
+class FacturaVentaResource(Resource):
+    @nsFacturaVenta.marshal_with(modeloFacturaVenta)
+    def get(self, id):
+        p = repo.get_by_id(id)
+        if p:
+            return p, 200
+        abort(404)
+    
+    
+    
+    # @nsFacturaVenta.expect(modeloFacturaVenta)
+    # def put(self, id):
+    #     data = editarFacturaVentaParser.parse_args()
+    #     if repo.modificar(id,data):
+    #         return 'Factura de Venta actualizada', 200
+    #     abort(404)
+   
+
+# @nsFacturaVenta.route('/buscar/<string:desde>/<string:hasta>/')
+# class FacturaVentaResource(Resource):
+#     @nsFacturaVenta.marshal_list_with(modeloFacturaVenta)
+#     def get(self, desde, hasta):
+#         l = repo.buscar(desde, hasta)
+#         if l:
+#             return l, 200
+#         abort(404)
+
+
+# @nsFacturaVenta.route('/baja/<int:id>')
+# class FacturaVentaResource(Resource):
+#     @nsFacturaVenta.expect(modeloFacturaVenta)
+
+#     def put(self, id):
+#         if repo.baja(id):
+#             repo.baja(id)
+                       
+#             return 'Detalle dado de Baja', 200            
+#         abort(400)
+        
--- backend/dominio/facturaVenta.py
+++ backend/dominio/facturaVenta.py
@@ -3,19 +3,22 @@
 # presupuesto.
 from sqlalchemy import Integer, ForeignKey, String, Column, Float
 from sqlalchemy.orm import relationship
+from flask_restx.fields import Date
 class FacturaVenta(db.Model):
 
     # codigo - descripcion - cantidad vendida - monto total (con iva)
     __tablename__ = 'facturas_venta'
     #Relacion con Articulo/Producto?
+    ### CORREGIR AUTOINCREMENT
     numero = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     tipo_id =relationship('TipoFactura', foreign_keys='Factura.tipo.codigo')
     tipo =relationship('TipoFactura', backref='FacturaVenta', foreign_keys='FacturaVenta.tipo_id')
-    # vendedor = relationship('Vendedor', backref='FacturaVenta')
-    # cliente_id =relationship('Cliente', foreign_keys='Cliente.cuit')
-    # cliente =relationship('Cliente', backref='FacturaVenta')
+    vendedor_id= relationship('Vendedor', foreign_keys='Vendedor.codigo')
+    vendedor = relationship('Vendedor', backref='FacturaVenta')
+    cliente_id =relationship('Cliente', foreign_keys='Cliente.cuit')
+    cliente =relationship('Cliente', backref='FacturaVenta')
     detalle =  relationship('FacturaDetalle',foreign_keys='FacturaVenta.numero', backref='FacturaVenta')
     total = Column(Float(),True)
-
+    fecha = Column(Date())
 
 
